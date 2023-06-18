3d69d0b621365481ee0c83848c5d9f9202e794c0
Vitor <vitorlzampronha@gmail.com>
2023-06-18 14:28:15 -0300

    classe posicao

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace xadrez_console
+﻿using xadrez_console.tabuleiro;
+using xadrez_console.tabuleiro;
+
+namespace xadrez_console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            posicao P = new posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+            Console.ReadLine();
         }
     }
 }
---
+++ tabuleiro/posicao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.tabuleiro
+{
+    internal class posicao
+    {
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+
+        }
+
+        public override string ToString()
+        {
+            return linha 
+                + ", " 
+                + coluna;
+        }
+
+    }
+}
